f72924f598a9d6f5e00c387c7b469483094ba838
Curtis <45854361+ExcentricEevee@users.noreply.github.com>
2025-03-22 13:51:29 -0400

    Refactor phonenum cleaning into method

--- lib/assignment.rb
+++ lib/assignment.rb
@@ -2,6 +2,21 @@ require 'csv'
 
 puts 'assignment.rb Initialized'
 
+def clean_phone(phone)
+  nums = phone.split("").reduce(Array.new) do |ary, char|
+    char.match?(/[0-9]/) ? ary.push(char) : ary
+  end
+
+  if nums.length == 10
+    nums.join
+  elsif nums.length == 11 && nums[0] == "1"
+    nums.shift
+    nums.join
+  else
+    "Invalid Phone Number"
+  end
+end
+
 contents = CSV.open(
   'event_attendees.csv',
   headers: true,
@@ -9,21 +24,5 @@ contents = CSV.open(
 )
 
 contents.each do |row|
-  phone = row[:homephone]
-
-  chars = phone.split("")
-  clean_phone = chars.reduce(Array.new) do |ary, char|
-    if char.match?(/[0-9]/)
-      ary.push(char)
-    else
-      ary
-    end
-  end
-
-  if clean_phone.length == 10
-    p clean_phone.join
-  elsif clean_phone.length == 11 && clean_phone[0] == "1"
-    clean_phone.shift
-    p clean_phone.join
-  end
+  p clean_phone(row[:homephone])
 end
